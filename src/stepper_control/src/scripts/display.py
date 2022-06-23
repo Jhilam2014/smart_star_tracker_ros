41165721ae9fbf3fe93d615a06c79d0c68c7e36a
Jhilam2014 <jhilam.astro@gmail.com>
2022-06-24 01:49:28 +0530

    changes in display node

--- src/stepper_control/src/scripts/display.py
+++ src/stepper_control/src/scripts/display.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from luma.core.interface.serial import i2c, spi, pcf8574
 from luma.core.interface.parallel import bitbang_6800
@@ -7,6 +7,7 @@ from luma.oled.device import ssd1306, ssd1309, ssd1325, ssd1331, sh1106, ws0010
 import rospy
 from std_msgs.msg import String
 import json
+import yaml
 
 serial = spi(device=0, port=0)
 device = sh1106(serial)
@@ -16,13 +17,14 @@ def displayMsg(data):
     with canvas(device) as draw:
         line = 10
         draw.rectangle(device.bounding_box, outline="white", fill="black")
-        displayData = json.loads(data.data)
+        rawMsg = data.data
+        displayData = yaml.safe_load(str(rawMsg))
+      
         for each in displayData:
             draw.text((20, line), str(each)+':'+str(displayData[each]), fill="white")
             line +=10     
 
 def listener():
-    rospy.init_node('displayNode', anonymous=True)
     rospy.Subscriber('disData',String, displayMsg)
     rospy.spin()
 
