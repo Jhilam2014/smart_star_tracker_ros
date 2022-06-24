35d2588de101a6a1a77d46a7143c4b5cc47daf15
Jhilam2014 <jhilambera@Jhilams-MacBook-Pro.local>
2022-06-24 19:30:47 +0530

    menu -

--- src/stepper_control/src/scripts/rosMain.py
+++ src/stepper_control/src/scripts/rosMain.py
@@ -82,9 +82,12 @@ def menuBoardCbk(data):
                 print(data.data+" in "+initMenu.currentPage)
                 query = loadPages[initMenu.breadcrm[-1]][0][str(data.data)] #the name of the object in the json => "1": (Speed)-?> query
                 if (query):
-                    initMenu.currentPage = str(data.data)
-                    pub.publish(str(loadPages[query][0]))
-                    initMenu.breadcrm.append(initMenu.currentPage)
+                    if(query in loadPages.keys()):
+                        initMenu.currentPage = query
+                        pub.publish(str(loadPages[query][0]))
+                        initMenu.breadcrm.append(initMenu.currentPage)
+                    else:
+                        pub.publish(str(loadPages[query][0]))
                 else: #set value
                     loadPages[initMenu.breadcrm[-1]][0][str(data.data)] += 10
                     pub.publish(str(loadPages[initMenu.currentPage][0]))
