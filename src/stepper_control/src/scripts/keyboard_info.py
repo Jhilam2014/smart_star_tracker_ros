c9eac14a9025344fde595887fc1084f5a63b475b
Jhilam2014 <jhilambera@Jhilams-MacBook-Pro.local>
2022-06-25 15:46:00 +0530

    motor code add

--- src/stepper_control/src/scripts/keyboard_info.py
+++ src/stepper_control/src/scripts/keyboard_info.py
@@ -3,6 +3,8 @@
 
 from ast import operator
 import json
+
+from pandas import concat
 import rospy
 from std_msgs.msg import String
 import time 
@@ -76,37 +78,44 @@ def menuBoardCbk(data):
                 initMenu.breadcrm.append(initMenu.currentPage)
                 pub.publish(str(loadPages[initMenu.currentPage][0]))
         else:
-            allInfoInPageKeys = loadPages[initMenu.currentPage][0].keys() #example 1,2
-            if(data.data in allInfoInPageKeys):
-                query = loadPages[initMenu.breadcrm[-1]][0][str(data.data)] #the name of the object in the json => "1": (Speed)-?> query
-                
-                regex = r"(?!.*\b(?:Run|Stop|Subtract|[\d]{1,})\b).*$"
-                matches = re.match(regex, str(query), re.M)
-
-                if (matches):
-                    initMenu.currentPage = query
-                    pub.publish(str(loadPages[query][0]))
-                    initMenu.breadcrm.append(initMenu.currentPage)
-                elif (query == "Run"):
-                    dt = loadPages
-                    dt["type"] = initMenu.currentPage
-                    pub_motor.publish(str(dt))
-                else: #set value
-                    value = loadPages[initMenu.breadcrm[-1]][0][str(data.data)]
+            if(initMenu.currentPage == 'Edit'):
+                loadPages[initMenu.currentPage][0]['Enter Value'] += data.data
+            elif(str(data.data) == "Ok"):
+                initMenu.currentPage = loadPages[initMenu.breadcrm[-1]]
+                pub.publish(str(loadPages[initMenu.breadcrm[-1]]))
+            else:
+                allInfoInPageKeys = loadPages[initMenu.currentPage][0].keys() #example 1,2
+                if(data.data in allInfoInPageKeys):
+                    query = loadPages[initMenu.breadcrm[-1]][0][str(data.data)] #the name of the object in the json => "1": (Speed)-?> query
                     
-                    if(str(data.data) == "#"):
-                        value = loadPages[initMenu.breadcrm[-1]][0][initMenu.lastOperation]
-                        loadPages[initMenu.breadcrm[-1]][0][initMenu.lastOperation] = operations(value,str(data.data),10)
-                    elif(str(data.data) == "Ok"):
-                        initMenu.editFlag = not bool(initMenu.editFlag)
-                        if(initMenu.editFlag == True):
-                            pub.publish(str(loadPages['Edit']))
-                    else:
-                        loadPages[initMenu.breadcrm[-1]][0][str(data.data)] = operations(value,str(data.data),10)
-                        initMenu.lastOperation = str(data.data)
-                   
+                    regex = r"(?!.*\b(?:Run|Stop|Subtract|[\d]{1,})\b).*$"
+                    matches = re.match(regex, str(query), re.M)
 
-                    pub.publish(str(loadPages[initMenu.currentPage][0]))
+                    if (matches):
+                        initMenu.currentPage = query
+                        pub.publish(str(loadPages[query][0]))
+                        initMenu.breadcrm.append(initMenu.currentPage)
+                    elif (query == "Run"):
+                        dt = loadPages
+                        dt["type"] = initMenu.currentPage
+                        pub_motor.publish(str(dt))
+                    else: #set value
+                        value = loadPages[initMenu.breadcrm[-1]][0][str(data.data)]
+                        
+                        if(str(data.data) == "#"):
+                            value = loadPages[initMenu.breadcrm[-1]][0][initMenu.lastOperation]
+                            loadPages[initMenu.breadcrm[-1]][0][initMenu.lastOperation] = operations(value,str(data.data),10)
+                        elif(str(data.data) == "Ok"):
+                            initMenu.editFlag = not bool(initMenu.editFlag)
+                            if(initMenu.editFlag == True):
+                                initMenu.currentPage = 'Edit'
+                                pub.publish(str(loadPages['Edit']))
+                        else:
+                            loadPages[initMenu.breadcrm[-1]][0][str(data.data)] = operations(value,str(data.data),10)
+                            initMenu.lastOperation = str(data.data)
+                    
+
+                        pub.publish(str(loadPages[initMenu.currentPage][0]))
 
     
 
