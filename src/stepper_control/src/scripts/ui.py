8eac7ee3b86edc43650efc2350811693e9623be2
Jhilam Bera <jhilam.astro@gmail.com>
2022-12-25 00:41:48 +0530

    ui dasboard html

--- src/stepper_control/src/scripts/ui.py
+++ src/stepper_control/src/scripts/ui.py
@@ -23,7 +23,7 @@ loadPages = json.loads(jsonString)
 @app.route('/run',methods = ['POST'])
 def run():
     data = request.data
-    data = json.loads(data['data'])
+    data = json.loads(data)
     # rospy.loginfo(data)
     # data["type"] = 'Speed Control'
     # pub_motor.publish(str(data))
