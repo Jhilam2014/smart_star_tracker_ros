4b45d5d2103d66fb4a396dc3e7df472d5f48bfd6
Jhilam Bera <jhilam.astro@gmail.com>
2022-12-25 03:43:14 +0530

    ui dasboard html

--- src/stepper_control/src/scripts/ui.py
+++ src/stepper_control/src/scripts/ui.py
@@ -21,7 +21,7 @@ loadPages = json.loads(jsonString)
 @app.route('/run',methods = ['POST'])
 def run():
     data = request.data
-    data_json = ast.literal_eval(json.loads(data))
+    data_json = ast.literal_eval(str(json.loads(data)))
     data_adjust = data_json['dif']
     data_adjust['Speed Control'][0]['1'] = data_json['speed']
     # pub_motor.publish(str(data_json))
